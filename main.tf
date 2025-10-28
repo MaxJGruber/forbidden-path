2216ee1e0d25eb9242af72adcac4f0f16f49e40c
MaxJGruber <maxjgruber@gmail>
2025-10-28 18:57:52 +0100

    Initial terraform setup

---
+++ main.tf
@@ -0,0 +1,16 @@
+# 1. Set region
+# 2. Provision instance
+
+provider "aws" {
+  region=var.region
+}
+
+resource "aws_instance" "ec2" {
+    ami = "ami-02d7ced41dff52ebc"
+    instance_type = "t2.micro"
+    key_name = var.key_pair_name
+
+  tags = {
+    Name = "Max-Instance"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+    type = string
+     default = "eu-west-3"
+}
+
+variable "key_pair_name" {
+    type = string
+    default = "paris-key-max"
+}
